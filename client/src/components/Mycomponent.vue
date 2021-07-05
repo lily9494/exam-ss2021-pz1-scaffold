a59ac78dd07947cc032377474d76c1ef98446fc0
B Kleinen <kleinen@infrastructure.de>
2021-07-06 00:24:05 +0200

    mevn generate - component

---
+++ client/src/components/Mycomponent.vue
@@ -0,0 +1,16 @@
+git <template>
+  <center>
+    <br />
+    <h1>This is the Mycomponent component</h1>
+  </center>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
